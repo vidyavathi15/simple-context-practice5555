22d02d87d0ecd6556c41dccf5ca3632e8d2796cd
vidyavathi <vidyavathi15@gmail.com>
2022-05-12 13:02:31 +0530

    simple-context practice22

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import React, {useState} from "react"
 
 
 const personObj1 = {
-  id:1, name: "vidya", salary: 200
+  id:1, name: "vidya", salary:100
 }
 export const userContext = React.createContext({personObj1, changeDetails: () => {}})
 const  App = () =>  {
